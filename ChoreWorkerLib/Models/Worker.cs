6e40bbb70ee7c9af79b83de14270abdf448aea93
Kjell Skogsrud <kjell@skogsrud.net>
2020-10-28 16:49:03 +0100

    Define a worker model

---
+++ ChoreWorkerLib/Models/Worker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChoreWorkerLib.Models
+{
+    public class Worker
+    {
+        public string Id = "";
+        public string Name = "";
+    }
+}
